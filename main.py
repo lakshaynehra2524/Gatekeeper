c193a033882c5640bf41a07f0c46d479b7fb84df
Lakshay <lakshaynehra2524@gmail.com>
2026-07-30 21:56:50 +0530

    Delete index route and static file handling
    Removed index route and static file mounting.

--- main.py
+++ main.py
@@ -59,9 +59,3 @@ def run_eval():
 
 
 # frontend 
-@app.get("/", include_in_schema=False)
-def index():
-    return FileResponse(FRONTEND / "index.html")
-
-
-app.mount("/static", StaticFiles(directory=FRONTEND), name="static")
